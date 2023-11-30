0a92094792ccde9e0018418bee2229a4f3c080ae
AdrianLicko <licko.adrian03@gmail.com>
2023-11-30 22:45:07 +0100

    OPRAVA: upraveny subor Hry, Hra vie spawnut Centipede a prekazky v gride, centipede sa da hybat sipkami

--- Centipede/Hra.java
+++ Centipede/Hra.java
@@ -1,10 +1,14 @@
 import fri.shapesge.Manazer;
 import java.util.ArrayList;
+import java.util.Random;
 
 public class Hra {
     private Platno platno;
-    private Centipede centipede;
     private Manazer manazer;
+    private Centipede centipede;
+    private ArrayList<Prekazka> kamen;
+    private int pocetPrekazok;
+    private Random random;
     
     public Hra() {
         //this.platno = new Platno();
@@ -14,5 +18,31 @@ public class Hra {
         
         this.manazer = new Manazer();
         this.manazer.spravujObjekt(this.centipede);
+        
+        this.kamen = new ArrayList<Prekazka>();
+        this.pocetPrekazok = 40;
+    }
+    
+    public void pridajPrekazky() {
+        this.random = new Random();
+        
+        for (int i = 0; i < this.pocetPrekazok; i++) {
+            int suradnicaX;
+            int suradnicaY;
+            
+            for (;;) {
+                suradnicaX = random.nextInt(780); //sbge.ini Width - 20
+                if (suradnicaX % 20 == 0)
+                    break;
+            }
+            
+            for (;;) {
+                suradnicaY = random.nextInt(680); //sbge.ini Height - 20
+                if (suradnicaY % 20 == 0)
+                    break;
+            }
+            
+            this.kamen.add(new Prekazka(suradnicaX, suradnicaY));
+        }
     }
 }

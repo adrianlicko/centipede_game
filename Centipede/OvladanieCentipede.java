c82bb377f92b2536cfcbb9f59e4b65f941d7ddd6
AdrianLicko <licko.adrian03@gmail.com>
2023-11-30 23:07:56 +0100

    zaciatok OvladanieCentipede, preskok na upravu pouzivania enumov. Doteraz boli v Centipede na urcovanie smeru no treba ich vo viacerych triedach

--- Centipede/OvladanieCentipede.java
+++ Centipede/OvladanieCentipede.java
@@ -1,6 +1,28 @@
-public class OvladanieCentipede {
+import java.util.ArrayList;
+import java.util.Random;
 
-    public OvladanieCentipede() {
+public class OvladanieCentipede {
+    public Centipede centipede;
+    private Random random;
+    
+    public OvladanieCentipede(int dlzka) {
+        this.centipede = new Centipede(dlzka);
+        this.centipede.vykresli();
+    }
+    
+    public void ovladanieCentipedePocitacom() {
+        this.random = new Random();
         
+        this.centipede.posunDole();
+        for (;;) {
+            int smer = random.nextInt(4); // 0-Hore, 1-Vpravo, 2-Dole, 3-Vlavo
+            switch (smer) {
+                case 0:
+                    
+                case 1:
+                case 2:
+                case 3:
+            }
+        }
     }
 }

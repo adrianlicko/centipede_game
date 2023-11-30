dc6854d958b326cedfd96e423ea8409266425e9a
AdrianLicko <licko.adrian03@gmail.com>
2023-11-30 22:36:34 +0100

    Fungujuce pohyby Centipede, spawnutie prekazok, zacatie riesenia automatickeho ovladania Centipede

--- Centipede/Hra.java
+++ Centipede/Hra.java
@@ -1,6 +1,18 @@
-public class Hra {
+import fri.shapesge.Manazer;
+import java.util.ArrayList;
 
+public class Hra {
+    private Platno platno;
+    private Centipede centipede;
+    private Manazer manazer;
+    
     public Hra() {
+        //this.platno = new Platno();
         
+        this.centipede = new Centipede(5);
+        this.centipede.vykresli();
+        
+        this.manazer = new Manazer();
+        this.manazer.spravujObjekt(this.centipede);
     }
 }
